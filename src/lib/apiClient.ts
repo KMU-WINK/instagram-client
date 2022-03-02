27c192ea72a22a97d61d96a7fa3a7cd0f48d3a82
sirldev <sirl0@naver.com>
2022-03-03 00:47:03 +0900

    update apiClient.ts

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-	baseURL: "",
+	baseURL: process.env.NODE_ENV === "development" ? "" : "http://api.redesigninsta.kro.kr/",
 });
 
 export default apiClient;
